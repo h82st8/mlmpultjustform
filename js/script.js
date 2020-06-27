849325f25f1c75ad2cca27f771c5de9eec49311d
Ilya Shurmanov <h82st8@gmail.com>
2020-06-27 05:44:38 +0300

    первый

---
+++ js/script.js
@@ -0,0 +1,39 @@
+onload = function () {
+
+	// --------------------Кнопки языков-------------
+
+	let languages__wrap = document.querySelector(".languages__wrap");
+	languages__wrap.addEventListener("click", function (event) {
+		if (event.target.className === 'languages__button') {
+			let languages__button = document.querySelectorAll('.languages__button');
+			for (let i = 0; i < languages__button.length; i++) {
+				languages__button[i].classList.remove('languages__button--active');
+			}
+			event.target.classList.add('languages__button--active');
+		}
+	});
+
+	// --------------------Табы----------------------
+
+	let tabButton__wrap = document.querySelector(".tab_button__wrap");
+	tabButton__wrap.addEventListener("click", function (event) {
+		if (event.target.className === 'tab_button') {
+			let tabButton = document.querySelectorAll('.tab_button');
+			for (let i = 0; i < tabButton.length; i++) {
+				tabButton[i].classList.remove('tab_button--active');
+			}
+			event.target.classList.add('tab_button--active');
+
+			let tabBody = document.querySelectorAll('.tab_body');
+			let dataTab = event.target.getAttribute('data-tab');
+			for (let i = 0; i < tabBody.length; i++) {
+				if (dataTab == i) {
+					tabBody[i].classList.add('tab_body--active');
+				} else {
+					tabBody[i].classList.remove('tab_body--active');
+				}
+			}
+		}
+	});
+
+};
